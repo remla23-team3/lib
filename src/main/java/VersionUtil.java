abb2ad3131165eb24fe895806a577c38d8a5708d
Simcha Vos <simchavos.bedrijven@gmail.com>
2023-05-12 00:13:27 +0200

    Fix recursion issue

--- src/main/java/VersionUtil.java
+++ src/main/java/VersionUtil.java
@@ -4,11 +4,11 @@ public class VersionUtil {
 	private static final Logger logger = Logger.getLogger(VersionUtil.class.getName());
 
 	public static void main(String[] args) {
-		getVersion();
+		String version = VersionUtil.getVersion();
+		logger.info("The version of lib is " + version + ".");
 	}
 
 	public static String getVersion() {
-		String version = VersionUtil.getVersion();
-		logger.info("The version of lib is " + version + ".");
-		return System.getenv().get("CURRENT_VERSION"); 	}
+		return System.getenv().get("CURRENT_VERSION");
+	}
 }
