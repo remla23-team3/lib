b1f1ef75fe0b32cc73a1d7d1627927197292e267
Simcha Vos <simchavos.bedrijven@gmail.com>
2023-05-12 00:01:08 +0200

    Restore main method for testing

--- src/main/java/VersionUtil.java
+++ src/main/java/VersionUtil.java
@@ -3,6 +3,10 @@ import java.util.logging.Logger;
 public class VersionUtil {
 	private static final Logger logger = Logger.getLogger(VersionUtil.class.getName());
 
+	public static void main(String[] args) {
+		getVersion();
+	}
+
 	public static String getVersion() {
 		String version = VersionUtil.getVersion();
 		logger.info("The version of lib is " + version + ".");
