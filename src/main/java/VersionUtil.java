55d5c597bf085cb46b5295568901f50f93994279
Simcha Vos <simchavos.bedrijven@gmail.com>
2023-05-11 23:34:34 +0200

    Minor change to VersionUtil

--- src/main/java/VersionUtil.java
+++ src/main/java/VersionUtil.java
@@ -3,11 +3,8 @@ import java.util.logging.Logger;
 public class VersionUtil {
 	private static final Logger logger = Logger.getLogger(VersionUtil.class.getName());
 
-	public static void main(String[] args) {
+	public static String getVersion() {
 		String version = VersionUtil.getVersion();
 		logger.info("The version of lib is " + version + ".");
-	}
-
-	public static String getVersion() {
 		return System.getenv().get("CURRENT_VERSION"); 	}
 }
