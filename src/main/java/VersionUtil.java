ac28da3330f729c588240edf97f8de50290ee1b0
Ne184 <64256244+Ne184@users.noreply.github.com>
2023-05-12 17:43:28 +0200

    added dependency for the version and change build to include dependencies when building the jar

--- src/main/java/VersionUtil.java
+++ src/main/java/VersionUtil.java
@@ -1,14 +1,17 @@
 import java.util.logging.Logger;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class VersionUtil {
 	private static final Logger logger = Logger.getLogger(VersionUtil.class.getName());
 
 	public static void main(String[] args) {
 		String version = VersionUtil.getVersion();
-		logger.info("The version of lib is " + version + ".");
+		logger.info("The version of lib is " + version + ". ");
 	}
 
 	public static String getVersion() {
-		return System.getenv().get("CURRENT_VERSION");
+		Dotenv dotenv = Dotenv.configure().directory("./").load();
+		return dotenv.get("CURRENT_VERSION");
 	}
 }
